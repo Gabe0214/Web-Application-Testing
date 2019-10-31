83e00048654527f0176354642969040b4595a9d7
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-31 15:23:08 -0700

    imported Display in App

--- test/src/App.js
+++ test/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     
-    <div>Testing</div>
+    <div>    
+    <Dashboard/>
+    </div>  
   );
 }
 
